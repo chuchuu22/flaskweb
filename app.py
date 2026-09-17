c88fb7a6e3659edea1d6dc113609e203e655d9e2
DESKTOP-BBH2SST\USER <bangjiwoojiwoo@gmail.com>
2026-09-17 10:23:25 +0900

    app.py

--- app.py
+++ app.py
@@ -11,5 +11,11 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+@app.route('/memo', method=['GET'])
+def listening():
+    articles = list(db.articles.fine({}, {'_id:False'}))
+    return jsonify
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
